e9bb840670d3f12440e1648f60fc076c0c29d7e5
Geoff Smith <smith260194@gmail.com>
2019-07-02 08:50:30 +0100

    Adding activitie list view | adding indexeddb call on startup to retrieve indexdb value from the database to pass into component

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
-import React, {useState} from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
 import Homepage from './components/Homepage';
 import AddActivity from './components/AddActivity';
+import Activities from './components/Activities';
 import {openDB} from 'idb';
 
-const storeActivity = async (activity) => {
-    const dbName = 'activities';
-    const storeName = 'fieldNote';
+const initDatabase = async () => {
+    const dbName = 'fieldNote';
     const version = 1;
 
     const db = await openDB(dbName, version, {
         upgrade(db, oldVersion, newVersion, transaction) {
-            db.createObjectStore(storeName, {autoIncrement: true})
+            db.createObjectStore('activities',
+                {autoIncrement: true})
         }
-    })
+    });
+    return db;
+};
 
-    const tx = await db.transaction(storeName, 'readwrite');
-    const store = await tx.objectStore(storeName);
+const initActivities = async () => {
+    const db = await initDatabase();
+    const tx = await db.transaction('activities', 'readonly');
+    const activities = tx.objectStore('activities').getAll();
+    await tx.done;
+    return activities;
+};
+
+const storeActivity = async (activity) => {
+    const db = await initDatabase();
+
+    const tx = await db.transaction('activities', 'readwrite');
+    const store = await tx.objectStore('activities');
 
     await store.put(activity);
     await tx.done;
 };
 
 const App = () => {
-    const [screen, setScreen] = useState('homepage');
+    const [screen, setScreen] = useState('activities');
+    const [activities, setActivities] = useState([]);
+
+    useEffect(() => {
+        (async () => {
+           const activities = await initActivities();
+           setActivities(activities);
+       })();
+    },[]);
 
     return (
         <div className="App">
             {screen === 'homepage' && <Homepage setScreen={setScreen}/>}
             {screen === 'addActivity' && <AddActivity storeActivity={storeActivity}/>}
+            {screen === 'activities' && <Activities activities={activities}/>}
         </div>
     );
 }
---
+++ src/components/Activities.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const Activities = ({activities })  => {
+    return (
+        <div className="ActivitiesList">
+            <ul className="List">
+                {activities.map((activity, index) => {
+                    return <li key={index}>{activity.title}</li>
+                })}
+            </ul>
+            <div className="Detail">
+
+            </div>
+        </div>
+    );
+};
+
+export default Activities;
