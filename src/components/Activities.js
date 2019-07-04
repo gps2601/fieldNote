6621e906ef4932be76fe9d794e175c5db7bb8e8e
Geoff Smith <smith260194@gmail.com>
2019-07-04 10:35:01 +0100

    Adding some stylings to Actvitiy List

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -1,18 +1,58 @@
-import React from 'react';
+/** @jsx jsx */
+import {css, jsx} from '@emotion/core'
 
-const Activities = ({activities })  => {
+const Activities = ({activities}) => {
     return (
-        <div className="ActivitiesList">
-            <ul className="List">
+        <div className="Activities" css={css`
+        display: grid;
+        grid-template-columns: 300px auto;
+        grid-template-areas: "sidebar-desktop main";
+        width: 100vw;
+        height: 100vh;
+        @media (max-width: 800px) {
+            grid-template-columns: 80px auto;
+            grid-template-areas: "sidebar-mobile main";
+        }
+        `}>
+            <ul className="List" css={css`
+               grid-area: sidebar-desktop;
+               border-right: 1px solid black;
+               height: 100%;
+               text-align: left;
+               list-style-type: none;
+               @media (max-width: 800px) {
+                display: none;
+               }
+                `}>
                 {activities.map((activity, index) => {
-                    return <li key={index}>{activity.title}</li>
+                    return <li key={index} css={css`
+                    padding: 20px;
+                    border-bottom: 1px solid black;
+                    &:hover {
+                        background-color: lightgray;
+                        cursor: pointer;
+                     }
+                    `}>{activity.title}</li>
                 })}
             </ul>
-            <div className="Detail">
-
+            <ul css={css`
+               grid-area: sidebar-mobile;
+               border-right: 1px solid black;
+               height: 100%;
+               text-align: left;
+               list-style-type: none;
+               @media (min-width: 800px) {
+                display: none;
+               }
+                `}>
+                hello
+            </ul>
+            <div className="Detail" css={css`
+               grid-area: main;
+               `}>
             </div>
         </div>
-    );
-};
+    )
+}
 
-export default Activities;
+export default Activities
